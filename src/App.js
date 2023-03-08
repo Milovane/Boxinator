e707c20aa127fcd1b53c0e57e09dc0fc98a9086b
AliHabesh <ali.habesh0068@stud.hkr.se>
2023-03-08 10:39:37 +0100

    designed the login page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { User } from "./models/User";
+import LoginForm from "./components/LoginForm";
+import Navbar from "./components/Navbar";
 
 function App() {
-  console.log(new User("Me"));
-  return <div className="App"></div>;
+  return (
+    <>
+      <Navbar></Navbar>
+      <LoginForm></LoginForm>
+    </>
+  );
 }
 
 export default App;
