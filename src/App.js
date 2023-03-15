9edeb541793ea861dfba0e6edc90eadf550a7609
Milovane <122837001+Milovane@users.noreply.github.com>
2023-03-15 09:16:03 +0100

    Feat: added registerform

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { RequireAuth } from "react-auth-kit";
 import { HistoryPage } from "./pages/HistoryPage";
 import { ShipmentPage } from "./pages/ShipmentPage";
+import React from 'react';
+import RegisterForm from './RegisterForm';
 
 function App() {
   return (
@@ -14,6 +16,7 @@ function App() {
         <Route path="/" element={<LoginFormPage />}></Route>
         <Route path="/login" element={<LoginFormPage />}></Route>
         <Route path="/shipment" element={<ShipmentPage />}></Route>
+        <Route path="/register" element={<RegisterForm />}></Route>
         <Route
           path="/history"
           element={
