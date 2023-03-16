d0a2e9fa27c2c89ed588190149a42dd65cba83c0
Milovane <122837001+Milovane@users.noreply.github.com>
2023-03-16 16:31:36 +0100

    Mui import fix

--- src/components/ShipmentTable.js
+++ src/components/ShipmentTable.js
@@ -1,5 +1,5 @@
 import React from "react";
-import DeleteIcon from "@mui/icons-material/Delete";
+//import DeleteIcon from "@mui/icons-material/Delete";
 import IconButton from "@mui/material/IconButton";
 import {
   Table,
